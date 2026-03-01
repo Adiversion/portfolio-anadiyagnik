74b591f752e5227df1b055663ae7cd982940d167
Anadi S Yagnik <99112612+Adiversion@users.noreply.github.com>
2026-03-01 18:28:37 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://portfolio.anadisyagnik.workers.dev',
+  site: 'https://portfolio.pages.dev',
   integrations: [sitemap()],
   output: 'static',
 });
